34eae4b70ce65ba93b342153c73355a2c397a41d
sjjung <easter0113@gmail.com>
2015-04-27 17:47:21 +0900

    login

--- src/main/java/com/webapp/controller/LoginController.java
+++ src/main/java/com/webapp/controller/LoginController.java
@@ -1,21 +1,66 @@
+//http://localhost:8080/EmpWeb/webapp/login/login
+
 package com.webapp.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.webapp.model.User;
 
+@Controller
+@RequestMapping("/login")
 public class LoginController {
+	
+	static Log log = LogFactory.getLog(LoginController.class);
+	
 
-	public String login() {
+	@RequestMapping(value="/login", method=RequestMethod.GET)
+	public String login() { // 로그인 화면
 		
-		return "login/loginform";
+		log.info("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+		log.info("login() GET.....");
+		log.info("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
 		
 		
+		return "/login/loginform";
 	}
 	
-	public String login(User user) {
+	@RequestMapping(value="/login", method=RequestMethod.POST)
+	public String login(User user, HttpSession session) { // 로그인 처리
 		
-		return "login/success";
+		log.info("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+		log.info("login() POST.......");
+		log.info("id = " + user.getId());
+		log.info("password = " + user.getPassword());
+		log.info("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
 		
 		
+		if("webapp".equals(user.getId()) && "1234".equals(user.getPassword())) { // 로그인 성공
+			
+			session.setAttribute("user", user);
+			return "login/success";
+			
+		} else { // 로그인 실패
+			
+			return "redirect:login";
+		}
+
+	}
+	
+	
+	
+	
+	@RequestMapping(value="/logout", method=RequestMethod.GET)
+	public String logout(HttpSession session){ // 로그아웃
+		
+		session.invalidate(); // 세션 scope가 사라진다
+		
+		return "login/logoutsuccess";
 	}
 	
 	
---
+++ src/main/webapp/WEB-INF/login/loginform.jsp
@@ -0,0 +1,23 @@
+<!-- http://localhost:8080/EmpWeb/webapp/login/login -->
+
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>loginform.jsp</title>
+</head>
+<body>
+
+
+<form action="login" method="post">
+
+ID : <input type="text" name="id"/> <br>
+PASSWORD : <input type="password" name="password"/> <br>
+<input type="submit" value="Login">
+
+</form>
+
+</body>
+</html>
